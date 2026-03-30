f2b7307d1572342cbe583de902ce6dd05941c0b4
Levi Maciel <l3trol007@gmail.com>
2026-03-29 22:25:34 -0300

    VAI LOGO

---
+++ dinheiro.c
@@ -0,0 +1,11 @@
+int calcular_moedas(int moedas)
+{
+    if (moedas <= 4)
+    {
+        return moedas;
+    }
+    else 
+    {
+        return 100;
+    }
+}
---
+++ dinheiro.h
@@ -0,0 +1,6 @@
+#ifndef DINHEIRO_H
+#define DINHEIRO_H
+
+int calcular_moedas(int moedas);
+
+#endif
